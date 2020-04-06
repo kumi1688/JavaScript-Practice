1cd28e32d268c0d813b6875c4cd0e08fa0bd1b38
kumi1688 <kumi1688@ajou.ac.kr>
2020-04-06 17:09:43 +0900

    tip3 before

---
+++ tip3/scope.js
@@ -0,0 +1,13 @@
+//var은 함수의 유효범위를 따른다. 따라서 함수에서 변수에 마지막으로 할당된 값을 참조한다
+function addClick(items) {
+  for (var i = 0; i < items.length; i++) {
+    items[i].onClick = function() {
+      return i;
+    };
+  }
+  return items;
+}
+
+const example = [{}, {}];
+const clickSet = addClick(example);
+console.log(clickSet[0].onClick());
